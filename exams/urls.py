4e86cde914dd18216235d6f6e963213f5a08ec5e
Rafael S. Gomes <rsg5775@gmail.com>
2025-11-30 19:03:23 -0300

    lista de exames feita

--- exams/urls.py
+++ exams/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.exams_list, name='exams_list'),
+    path("add/", views.add_exam, name='add_exam'),
 ]
--- exams/views.py
+++ exams/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from datetime import datetime
+import re
 
-from .models import Exams
+from .models import Exams, Patients, Medics
 
 def exams_list(request):
     exams = Exams.objects.select_related("patient", "medic").all()
@@ -15,13 +16,58 @@ def get_exam(request, id):
         "exam": exam
     })
 
+def verify_form(exam_type, patient, medic, scheduled_exam, exam_status, exam_value, pay_status):
+
+    if not exam_type not in dict(Exams.EXAM_TYPES):
+        return "Tipo de exame inválido ou não fazemos esse tipo de exame."
+    
+    if not Patients.objects.filter(id=patient.id).exists():
+        return "Paciente inválido"
+    
+    if not Medics.objects.filter(id=medic.id).exists():
+        return "Médico inválido"
+    
+    if not isinstance(scheduled_exam, datetime):
+        return "Data agendada inválida"
+    
+    now = datetime.now()
+    if scheduled_exam < now:
+        return "A data agendada não pode estar no passado"
+    
+    if exam_status not in dict(Exams.STATUS_CHOICES):
+        return "Status inválido"
+    
+    try:
+        exam_value = float(exam_value)
+        if exam_value < 0:
+            return "O valor não pode ser negativo"
+    except:
+        return "Valor do exame inválido"
+    
+    if pay_status not in dict(Exams.PAYMENT_STATUS):
+        return "Status de pagamento inválido."
+    
+    return None
+
 def add_exam(request):
-    succes = False
+    success = False
     error = None
 
     if request.method == 'POST':
         e_type = request.POST.get("e_type")
         patient = request.POST.get("patient")
         medic = request.POST.get("medic")
-        schl_e = request.POST.get("")
+        scdl = request.POST.get("scdl")
+        e_status = request.POST.get("e_status")
+        e_value = request.POST.get("e_value")
+        pay_status = request.POST.get("pay_status")
 
+        error = verify_form(e_type, patient, medic, scdl, e_status, e_value, pay_status)    
+        
+        if not error:
+            Exams.create_exam(e_type, patient, medic, scdl, e_status, e_value, pay_status)
+            success = True
+
+    return render(request, "exams/add_exam.html", {
+        "error": error, "success": success
+    })
